453e36036b13e3a8ad6131f182b4c7a91b6e89a6
Camilo Vargas <camilovargasingm@gmail.com>
2022-10-05 22:51:54 -0500

    add action delete post

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,14 +1,12 @@
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-import { useEffect, useState } from "react";
+import { DeleteForever, Edit } from '@mui/icons-material';
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import { useEffect, useMemo, useState } from "react";
 import { PostPreview } from "src/models";
-import getListPost from "./services/getPosts";
+import { Buttons } from "src/styled-components/buttons.styled.components";
+import { deletePost, getListPost } from "./services";
+import { ButtonsActions } from "./styled-components";
 
-const columns: GridColDef[] = [
-	{ field: 'id', headerName: 'ID', width: 100 },
-	{ field: 'userId', headerName: 'User ID', width: 100 },
-	{ field: 'body', headerName: 'Cuerpo', minWidth: 130, flex: 1 },
-	{ field: 'title', headerName: 'Titulo', type: 'number', minWidth: 130, flex: 1 }
-];
+
 
 export default function Home() {
 	const pageSize = 5
@@ -24,6 +22,30 @@ export default function Home() {
 		getPost()
 	}, [])
 
+	function handleDelete(id: number) {
+		return deletePost(id).then(() => setPost(post => post.filter(item => item.id !== id))).catch(() => console.error("Error to delete"))
+	}
+
+	const columns: GridColDef[] = useMemo(() => [
+		{ field: 'id', headerName: 'ID', width: 100 },
+		{ field: 'userId', headerName: 'User ID', width: 100 },
+		{ field: 'body', headerName: 'Cuerpo', minWidth: 130, flex: 1 },
+		{ field: 'title', headerName: 'Titulo', type: 'number', minWidth: 130, flex: 1 },
+		{
+			field: 'actions', headerName: 'Acciones', type: 'actions', sortable: false, width: 150,
+			renderCell: (items: GridRenderCellParams) => (
+				<ButtonsActions>
+					<Buttons variant="edit" onClick={() => console.log(items.id)} >
+						<Edit />
+					</Buttons>
+					<Buttons variant="delete" onClick={() => handleDelete(items.id as number)} >
+						<DeleteForever />
+					</Buttons>
+				</ButtonsActions>
+			)
+		}
+	], [])
+
 	return (
 		<DataGrid
 			disableColumnSelector
@@ -33,7 +55,6 @@ export default function Home() {
 			columns={columns}
 			pageSize={pageSize}
 			rowsPerPageOptions={[rowPerPage]}
-			getRowId={(row => row.id)}
 		/>
 	)
 }
