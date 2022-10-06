6c5b41ece42fa7f533ceafdc090635ac1ec88544
Camilo Vargas <camilovargasingm@gmail.com>
2022-10-05 21:09:23 -0500

    add models of posts

---
+++ src/models/post.type.ts
@@ -0,0 +1,13 @@
+export interface ApiPostPreview {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+export interface PostPreview {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
