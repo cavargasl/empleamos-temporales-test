09bef08350ba9368c619da599976092f1c7d1e5a
Camilo Vargas <camilovargasingm@gmail.com>
2022-10-06 20:22:13 -0500

    add enpoint

--- src/page/Home/components/services/crudPost.ts
+++ src/page/Home/components/services/crudPost.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { PostPreview } from "src/models";
+import { ApiPostPreview, PostPreview } from "src/models";
 import { getPostAdapter } from "../adapter/getPost.adapter";
 
 export async function getListPost(): Promise<PostPreview[]> {
@@ -14,6 +14,10 @@ interface UpdatePost {
   data: Omit<PostPreview, "id" | "userId">
   id: PostPreview["id"]
 }
-export async function updatePost({ id, data }: UpdatePost) {
-  return axios.patch(`/posts/${id}`, { ...data })
+export async function updatePartialPost({ id, data }: UpdatePost) {
+  return axios.patch(`/posts/${id}`, { title: data.title, body: data.body })
+}
+
+export async function createPost(data: PostPreview) {
+  return axios.post('/posts', data)
 }
